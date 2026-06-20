704a1fb9b75c3cdacfc438fbebaad8dcb21c3d63
evan <1213922281@qq.com>
2026-06-20 13:21:40 +0800

    Update download URL for LocalMind setup

--- assets/config.js
+++ assets/config.js
@@ -2,4 +2,4 @@
 // 方案 A：把安装包放到本页面同级的 downloads 文件夹，保持下面这个相对地址。
 // 方案 B：换成腾讯云 COS/对象存储直链，要求免登录、无广告、点击即下载。
 window.LOCALMIND_DOWNLOAD_URL =
-"https://github.com/evan12139/localMind/releases/latest/download/LocalMind-1.0.0-setup.exe";
+"https://github.com/Evancreates/localMind/releases/latest/download/LocalMind-1.0.0-setup.exe";
